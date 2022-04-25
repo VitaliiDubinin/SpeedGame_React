343164ae2ed41d0a6e90bd881584d0c2033455f0
VitaliiDubinin <vitalii.dubinin@edu.bc.fi>
2022-04-25 21:53:03 +0300

    build added+

---
+++ build/precache-manifest.4ad8bdd6ff0643e188cf827f46c830c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "379ba77329dc9626a76c",
+    "url": "/SpeedGame_React/static/css/main.04a59d45.chunk.css"
+  },
+  {
+    "revision": "379ba77329dc9626a76c",
+    "url": "/SpeedGame_React/static/js/main.766f75d7.chunk.js"
+  },
+  {
+    "revision": "5d6223458c990f187322",
+    "url": "/SpeedGame_React/static/js/runtime~main.bc8de228.js"
+  },
+  {
+    "revision": "5c8ae712ee8906145562",
+    "url": "/SpeedGame_React/static/js/2.91805d08.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/SpeedGame_React/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "559602effb631d0bec5a0284d10827b2",
+    "url": "/SpeedGame_React/static/media/02 Story.559602ef.flac"
+  },
+  {
+    "revision": "2017d0cd126bd2d525f1b55bfe335851",
+    "url": "/SpeedGame_React/static/media/01. Sega Intro.2017d0cd.flac"
+  },
+  {
+    "revision": "4946111dc2bf2115ccffe3ff93ec2bbd",
+    "url": "/SpeedGame_React/static/media/briefcase-lock-1.4946111d.wav"
+  },
+  {
+    "revision": "2e38f9a1feec9fd793e828482d10db30",
+    "url": "/SpeedGame_React/index.html"
+  }
+];
